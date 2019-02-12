89ea008984c565bd6a6c923521fb27953482954a
Guilhem Lettron <guilhem@barpilot.io>
2019-02-12 12:50:44 +0100

    import of aks cluster driver
    https://github.com/rancher/ui/tree/master/lib/shared/addon/components/cluster-driver/driver-azureaks

--- component/component.js
+++ component/component.js
@@ -2,6 +2,7 @@
 // https://github.com/rancher/ui/blob/master/lib/shared/addon/mixins/cluster-driver.js
 import ClusterDriver from 'shared/mixins/cluster-driver';
 
+
 // do not remove LAYOUT, it is replaced at build time with a base64 representation of the template of the hbs template
 // we do this to avoid converting template to a js file that returns a string and the cors issues that would come along with that
 const LAYOUT;
@@ -20,7 +21,19 @@ const all          = Ember.RSVP.all;
 
 /*!!!!!!!!!!!GLOBAL CONST END!!!!!!!!!!!*/
 
+const on            = Ember.on;
+const equal         = Ember.equal;
+const setProperties = Ember.setProperties;
 
+import ipaddr from 'ipaddr.js';
+import { hash } from 'rsvp';
+
+import {
+  sizes,
+  aksRegions,
+} from 'ui/utils/azure-choices';
+
+const NETWORK_POLICY = ['Calico']
 
 /*!!!!!!!!!!!DO NOT CHANGE START!!!!!!!!!!!*/
 export default Ember.Component.extend(ClusterDriver, {
@@ -29,6 +42,13 @@ export default Ember.Component.extend(ClusterDriver, {
   app:         service(),
   router:      service(),
 /*!!!!!!!!!!!DO NOT CHANGE END!!!!!!!!!!!*/
+
+  zones:                  aksRegions,
+  versions:               null,
+  machineSizes:           sizes,
+  step:                   1,
+  netMode:                'default',
+  monitoringRegionConent: [],
 
   init() {
     /*!!!!!!!!!!!DO NOT CHANGE START!!!!!!!!!!!*/
@@ -48,51 +68,255 @@ export default Ember.Component.extend(ClusterDriver, {
 
     if ( !config ) {
       config = this.get('globalStore').createRecord({
-        type:               configField,
+        agentPoolName:                'rancher',
+        type:                         'azureKubernetesServiceConfig',
+        agentOsdiskSize:              100,
+        adminUsername:                'azureuser',
+        kubernetesVersion:            '1.11.5',
+        count:                        3,
+        agentVmSize:                  'Standard_D2_v2',
+        location:                     'eastus',
+        enableHttpApplicationRouting: false,
+        enableMonitoring:             true,
       });
 
       set(this, 'cluster.%%DRIVERNAME%%EngineConfig', config);
+    } else {
+      const tags = get(config, 'tags') || []
+      const map = {}
+
+      tags.map((t = '') => {
+        const split = t.split('=')
+
+        set(map, split[0], split[1])
+      })
+      set(this, 'tags', map)
+
+      if (get(config, 'networkPolicy')) {
+        set(this, 'netMode', 'advanced')
+      }
     }
   },
 
   config: alias('cluster.%%DRIVERNAME%%EngineConfig'),
 
-
   actions: {
-    save() {},
-    cancel(){
-      // probably should not remove this as its what every other driver uses to get back
-      get(this, 'router').transitionTo('global-admin.clusters.index');
+    authenticate(cb) {
+      const store = get(this, 'globalStore')
+      const data = {
+        clientId:       get(this, 'config.clientId'),
+        clientSecret:   get(this, 'config.clientSecret'),
+        subscriptionId: get(this, 'config.subscriptionId'),
+        tenantId:       get(this, 'config.tenantId'),
+        region:         get(this, 'config.location')
+      };
+      const aksRequest = {
+        versions: store.rawRequest({
+          url:    '/meta/aksVersions',
+          method: 'POST',
+          data
+        }),
+        virtualNetworks: store.rawRequest({
+          url:    '/meta/aksVirtualNetworks',
+          method: 'POST',
+          data
+        })
+      }
+
+      return hash(aksRequest).then((resp) => {
+        const { versions, virtualNetworks } = resp;
+
+        setProperties(this, {
+          step:            2,
+          versions:        (get(versions, 'body') || []),
+          virtualNetworks: (get(virtualNetworks, 'body') || []),
+        });
+
+        cb(true);
+      }).catch((xhr) => {
+        const err = xhr.body.message || xhr.body.code || xhr.body.error;
+
+        setProperties(this, { errors: [err], });
+
+        cb(false, [err]);
+      });
+    },
+
+    setTags(section) {
+      const out = []
+
+      for (let key in section) {
+        out.pushObject(`${ key }=${ section[key] }`)
+      }
+      set(this, 'config.tags', out);
     },
   },
 
+  resetAdvancedOptions: on('init', observer('netMode', function() {
+    if (get(this, 'netMode') === 'default') {
+      const config = get(this, 'config');
 
-  // Add custom validation beyond what can be done from the config API schema
+      setProperties(config, {
+        subnet:                      null,
+        virtualNetwork:              null,
+        virtualNetworkResourceGroup: null,
+        serviceCidr:                 null,
+        dnsServiceIp:                null,
+        dockerBridgeCidr:            null
+      });
+    }
+  })),
+
+  networkChoice: computed({
+    set( key, value = '' ) {
+      const [subnet, virtualNetwork, virtualNetworkResourceGroup] = value.split(':');
+      const config = get(this, 'config');
+
+      if (subnet && virtualNetwork && virtualNetworkResourceGroup) {
+        setProperties(config, {
+          subnet,
+          virtualNetwork,
+          virtualNetworkResourceGroup
+        });
+      }
+
+      return value;
+    }
+  }),
+
+  filteredVirtualNetworks: computed('config.virtualNetwork', 'virtualNetworks', function() {
+    const vnets = get(this, 'virtualNetworks');
+    const subNets = [];
+
+    vnets.forEach( (vnet) => {
+      get(vnet, 'subnets').forEach( (subnet) => {
+        subNets.pushObject({
+          name:  `${ get(subnet, 'name') } (${ get(subnet, 'addressRange') })`,
+          group: get(vnet, 'name'),
+          value: `${ get(subnet, 'name') }:${ get(vnet, 'name') }:${ get(vnet, 'resourceGroup') }`
+        })
+      });
+    });
+
+    return subNets;
+  }),
+
+  isEditable: computed('mode', function() {
+    return ( get(this, 'mode') === 'edit' || get(this, 'mode') === 'new' ) ? true : false;
+  }),
+
+  saveDisabled: computed('config.subscriptionId', 'config.tenantId', 'config.clientId', 'config.clientSecret', 'config.location', function() {
+    return get(this, 'config.tenantId') && get(this, 'config.clientId') && get(this, 'config.clientSecret') && get(this, 'config.subscriptionId') && get(this, 'config.location') ? false : true;
+  }),
+
+  networkPolicyContent: computed(() => {
+    return NETWORK_POLICY.map((n) => {
+      return {
+        label: n,
+        value: n,
+      }
+    })
+  }),
+
   validate() {
-    // Get generic API validation errors
-    this._super();
-    var errors = get(this, 'errors')||[];
-    if ( !get(this, 'cluster.name') ) {
-      errors.push('Name is required');
+    const intl   = get(this, 'intl');
+    let model = get(this, 'cluster');
+    let errors = model.validationErrors() || [];
+
+    const vnetSet = !!get(this, 'config.virtualNetwork');
+
+    if (vnetSet) {
+      errors = errors.concat(this.validateVnetInputs());
     }
 
-    // Add more specific errors
-
-    // Check something and add an error entry if it fails:
-    // if ( parseInt(get(this, 'config.memorySize'), defaultRadix) < defaultBase ) {
-    //   errors.push('Memory Size must be at least 1024 MB');
-    // }
-
-    // Set the array of errors for display,
-    // and return true if saving should continue.
-    if ( get(errors, 'length') ) {
-      set(this, 'errors', errors);
-      return false;
-    } else {
-      set(this, 'errors', null);
-      return true;
+    if ( !get(this, 'config.resourceGroup') ) {
+      errors.push(intl.t('validation.required', { key: intl.t('clusterNew.azureaks.resourceGroup.label') }));
     }
+
+    if ( !get(this, 'config.sshPublicKeyContents') ) {
+      errors.push(intl.t('validation.required', { key: intl.t('clusterNew.azureaks.ssh.label') }));
+    }
+
+    set(this, 'errors', errors);
+
+    return errors.length === 0;
   },
 
-  // Any computed properties or custom logic can go here
+  validateVnetInputs() {
+    const intl   = get(this, 'intl');
+    const errors = [];
+    const config = get(this, 'config');
+    const vnet   = get(this, 'virtualNetworks').findBy('name', get(config, 'virtualNetwork'));
+
+    if (vnet) {
+      let subnet = get(vnet, `subnets`).findBy('name', get(config, 'subnet'));
+      let vnetRange  = ipaddr.parseCIDR(get(subnet, 'addressRange'));
+
+      let {
+        serviceCidr, dnsServiceIp, dockerBridgeCidr
+      } = config;
+
+      let parsedServiceCidr      = null;
+      let parsedDnsServiceIp     = null;
+      let parsedDockerBridgeCidr = null;
+
+      if (!serviceCidr && !dnsServiceIp && !dockerBridgeCidr) {
+        errors.pushObject('You must include all required fields when using a Virtual Network');
+      }
+
+      try {
+        parsedServiceCidr = ipaddr.parseCIDR(serviceCidr);
+
+        // check if serviceCidr falls within the VNet/Subnet range
+        if (parsedServiceCidr && vnetRange[0].match(parsedServiceCidr)) {
+          errors.pushObject(intl.t('clusterNew.azureaks.errors.included.parsedServiceCidr'));
+        }
+      } catch ( err ) {
+        errors.pushObject(intl.t('clusterNew.azureaks.errors.included.serviceCidr'));
+      }
+
+      try {
+        parsedDnsServiceIp = ipaddr.parse(dnsServiceIp);
+
+        // check if dnsService exists in range
+        if (parsedDnsServiceIp && vnetRange[0].match(parsedDnsServiceIp, vnetRange[1])) {
+          errors.pushObject(intl.t('clusterNew.azureaks.errors.included.parsedDnsServiceIp'));
+        }
+      } catch ( err ) {
+        errors.pushObject(intl.t('clusterNew.azureaks.errors.included.dnsServiceIp'));
+      }
+
+      try {
+        parsedDockerBridgeCidr = ipaddr.parseCIDR(dockerBridgeCidr);
+
+        // check that dockerBridge doesn't overlap
+        if (parsedDockerBridgeCidr && ( vnetRange[0].match(parsedDockerBridgeCidr) || parsedServiceCidr[0].match(parsedDockerBridgeCidr) )) {
+          errors.pushObject(intl.t('clusterNew.azureaks.errors.included.parsedDockerBridgeCidr'));
+        }
+      } catch ( err ) {
+        errors.pushObject(intl.t('clusterNew.azureaks.errors.included.dockerBridgeCidr'));
+      }
+    }
+
+    return errors;
+  },
+
+  willSave() {
+    const enableMonitoring = get(this, 'config.enableMonitoring')
+    const config = get(this, 'config')
+
+    if (enableMonitoring) {
+      setProperties(config, {
+        logAnalyticsWorkspaceResourceGroup: '',
+        logAnalyticsWorkspace:              '',
+      })
+    } else {
+      setProperties(config, {
+        logAnalyticsWorkspaceResourceGroup: null,
+        logAnalyticsWorkspace:              null,
+      })
+    }
+
+    return this._super(...arguments);
+  },
 });
